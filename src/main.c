f62047b9383bcd5adcde1c40d61f714e87f8f44a
mfahadm8 <90320500+mfahadm8@users.noreply.github.com>
2023-03-30 15:50:25 +0500

    added vehicle safe distance braking logic

--- src/main.c
+++ src/main.c
@@ -1,24 +1,33 @@
-#include <stdio.h>
 #include <uncertain.h>
+#include <stdio.h>
+#include <math.h>
 
-int
-main(int argc, char *  argv[])
-{
-	double	a, b, c;
-
-	a = libUncertainDoubleUniformDist(0.5, 1.0);
-	printf("a = %lf\n", a);
-
-	b = libUncertainDoubleUniformDist(10.0, 20.0);
-	printf("b = %lf\n", b);
-
-	c = (a+b)/(a-b);
-	printf("c = %lf\n", c);
-
-#ifdef DEBUG
-	printf("debug message\n");
-#endif
-
-	return 0;
+double likelihood(double dist) {
+    double sigma = 2.0; // Measurement noise with standard deviation of 2
+    return libUncertainDoubleGaussDist(dist, sigma);
 }
 
+double calc_safe_distance(double speed, double mass, double friction_coeff) {
+    // Calculate the safe distance required to apply brakes
+    double reaction_time = libUncertainDoubleUniformDist(0.5, 1.5);
+    double prior_distance = libUncertainDoubleUniformDist(0.0, 80.0); // Uniform prior distribution of the distance to the obstacle
+    double measured_distance = 70; // Distance to the obstacle as measured by the sensor
+    double posterior_distance = libUncertainDoubleBayesLaplace(&likelihood, prior_distance, measured_distance); // Posterior distribution of the distance to the obstacle
+    
+    // Calculate the braking distance
+    double gravity = 9.81; // Acceleration due to gravity in m/s^2
+    double friction_force = mass * gravity * friction_coeff; // Friction force
+    double braking_distance = (speed * speed) / (2.0 * friction_force); // Braking distance in meters
+    
+    double safe_distance = reaction_time * speed + posterior_distance + braking_distance + 2.0; // Add safety buffer of 2 meters
+    return safe_distance;
+}
+
+int main() {
+    double speed = 30.0; // Speed of the autonomous vehicle in m/s (108 km/h)
+    double mass = 1500.0; // Mass of the vehicle in kg
+    double friction_coeff = 0.7; // Coefficient of friction between the tires and the road
+    double safe_distance = calc_safe_distance(speed, mass, friction_coeff);
+    printf("Vehicle can safely apply brakes at a distance of %lf meters.\n", safe_distance);
+    return 0;
+}
